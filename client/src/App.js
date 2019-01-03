91bc55ade289cc99db911d534bf06f115b4fd48e
Taro yamashita <taro.c.yamashita@gmail.com>
2019-01-02 21:24:37 -0500

    integrate apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import BookList from "./components/BookList";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
 import "./App.css";
+
+const client = new ApolloClient({
+  uri: "http://localhost:6069/graphql"
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <p> Taro's practice GraphQL Client </p>
-        <BookList />
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <p> Taro's practice GraphQL Client </p>
+          <BookList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
