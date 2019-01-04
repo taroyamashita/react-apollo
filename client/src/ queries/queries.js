6e57185cb281118b58a0a5f832dd60b35520797e
Taro yamashita <taro.c.yamashita@gmail.com>
2019-01-03 20:53:57 -0500

    created query file

---
+++ client/src/ queries/queries.js
@@ -0,0 +1,22 @@
+import { gql } from "apollo-boost";
+import { graphql } from "react-apollo";
+
+const getAuthorsQuery = gql`
+  {
+    authors {
+      name
+      id
+    }
+  }
+`;
+
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
+
+export { getAuthorsQuery, getBooksQuery };
