b7ba0f88d87478cb399cc46b2104efe0f0007f5b
Taro yamashita <taro.c.yamashita@gmail.com>
2019-01-07 20:33:11 -0500

    updated to dynamically render info

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -6,20 +6,35 @@ import { getBooksQuery } from "../queries/queries";
 import BookDetails from "./BookDetails";
 
 class BookList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: null
+    };
+  }
   displayBooks() {
     let data = this.props.data;
     if (data.loading) {
       return <div> Loading Books </div>;
     } else {
-      return data.books.map(book => <li key={book.id}> {book.name} </li>);
+      return data.books.map(book => (
+        <li
+          key={book.id}
+          onClick={e => {
+            this.setState({ selected: book.id });
+          }}
+        >
+          {" "}
+          {book.name}{" "}
+        </li>
+      ));
     }
   }
   render() {
-    console.log(this.props);
     return (
       <div>
         <ul id="book-list">{this.displayBooks()}</ul>
-        <BookDetails />
+        <BookDetails bookId={this.state.selected} />
       </div>
     );
   }
